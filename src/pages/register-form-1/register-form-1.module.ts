b7140c5a944aba426db32050651eb66971f65eac
santisanchez <santisanchez.1214@gmail.com>
2018-05-10 14:08:27 -0500

    Se agrega la vista de formulario de registro

---
+++ src/pages/register-form-1/register-form-1.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { RegisterForm_1Page } from './register-form-1';
+
+@NgModule({
+  declarations: [
+    RegisterForm_1Page,
+  ],
+  imports: [
+    IonicPageModule.forChild(RegisterForm_1Page),
+  ],
+})
+export class RegisterForm_1PageModule {}
