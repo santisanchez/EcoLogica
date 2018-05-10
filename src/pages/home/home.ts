15b5d0a780f570d1107c31fa5fdef32894458366
santisanchez <santisanchez.1214@gmail.com>
2018-05-10 14:05:44 -0500

    Se crea la vista de inicio

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,17 @@
+import { LoginPage } from './../login/login';
+import { RegisterPage } from './../register/register';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public register: any = RegisterPage;
+  public login: any = LoginPage;
+
 
   constructor(public navCtrl: NavController) {
 
