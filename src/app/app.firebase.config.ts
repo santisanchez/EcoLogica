b8dff03f691e640ca37017bd599ea284031615a8
santisanchez <santisanchez.1214@gmail.com>
2018-05-10 14:02:32 -0500

    Se crea la configuracion de firebase

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyDvqyt20CB3JByCzF2DioRO9m7my0RaL2U",
+    authDomain: "ecologica-10a81.firebaseapp.com",
+    databaseURL: "https://ecologica-10a81.firebaseio.com",
+    projectId: "ecologica-10a81",
+    storageBucket: "",
+    messagingSenderId: "977426524541"
+};
