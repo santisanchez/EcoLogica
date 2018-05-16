5ad002769808b5013fcb57d924bea3cf51ae82c7
santisanchez <santisanchez.1214@gmail.com>
2018-05-16 01:45:48 -0500

    Se agrega registro con firebase

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,9 +1,9 @@
+import { UserProvider } from './../../providers/user/user';
+import { HomePage } from './../home/home';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { RegisterForm_1Page } from '../register-form-1/register-form-1';
 import { User } from '../../modules/user';
-
-import { AngularFireAuth } from "angularfire2/auth";
 
 @IonicPage()
 @Component({
@@ -16,20 +16,22 @@ export class RegisterPage {
 
   public registerForm1: any = RegisterForm_1Page;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private afAuth: AngularFireAuth) {
+  public home: any = HomePage;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    private userProvider: UserProvider) {
   }
 
   register(user: User) {
     try {
-      const result = this.afAuth.auth.createUserWithEmailAndPassword(user.email, user.password);
-      console.log(result);
-      this.navCtrl.push(this.registerForm1);
+      this.userProvider.register(user);
+      this.navCtrl.push(this.home);
     } catch (e) {
       console.error(e);
 
     }
-
   }
+
 
   ionViewDidLoad() {
 
