9684f4e39b167eb4f5f813e260b2754abc1a30ea
santisanchez <santisanchez.1214@gmail.com>
2018-05-16 01:39:45 -0500

    Se agrega configuracion de firebase y paginas a las declaraciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,35 @@
+import { MainPage } from './../pages/main/main';
+import { TestPage } from './../pages/test/test';
+import { AchievementDetailPage } from './../pages/achievement-detail/achievement-detail';
+import { AchievementsPage } from './../pages/achievements/achievements';
 import { LoginPage } from './../pages/login/login';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+
 import { } from 'firebase'
-import { FIREBASE_CONFIG } from './app.firebase.config';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireStorageModule } from 'angularfire2/storage';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { RegisterPage } from '../pages/register/register';
 import { RegisterForm_1Page } from '../pages/register-form-1/register-form-1';
+import { StorageProvider } from '../providers/storage/storage';
+import { UserProvider } from '../providers/user/user';
+
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDvqyt20CB3JByCzF2DioRO9m7my0RaL2U",
+  authDomain: "ecologica-10a81.firebaseapp.com",
+  databaseURL: "https://ecologica-10a81.firebaseio.com",
+  projectId: "ecologica-10a81",
+  storageBucket: "ecologica-10a81.appspot.com",
+  messagingSenderId: "977426524541"
+};
 
 @NgModule({
   declarations: [
@@ -21,14 +37,19 @@ import { RegisterForm_1Page } from '../pages/register-form-1/register-form-1';
     HomePage,
     RegisterPage,
     RegisterForm_1Page,
-    LoginPage
+    LoginPage,
+    AchievementsPage,
+    AchievementDetailPage,
+    MainPage,
+    TestPage
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
-    AngularFireModule.initializeApp(FIREBASE_CONFIG),
+    AngularFireModule.initializeApp(FIREBASE_CONFIG, 'EcoLogica'),
     AngularFireDatabaseModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    AngularFireStorageModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -36,13 +57,19 @@ import { RegisterForm_1Page } from '../pages/register-form-1/register-form-1';
     HomePage,
     RegisterPage,
     RegisterForm_1Page,
-    LoginPage
+    LoginPage,
+    AchievementsPage,
+    AchievementDetailPage,
+    MainPage,
+    TestPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
     AngularFireDatabase,
-    { provide: ErrorHandler, useClass: IonicErrorHandler }
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    StorageProvider,
+    UserProvider
   ]
 })
 export class AppModule { }
