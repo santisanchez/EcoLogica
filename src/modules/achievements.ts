b47641f5ae8419129101760a7dbae178a06303b9
santisanchez <santisanchez.1214@gmail.com>
2018-05-16 01:40:25 -0500

    Se crea el arreglo AchievementData para los datos estaticos de los logros

---
+++ src/modules/achievements.ts
@@ -0,0 +1,16 @@
+export let AchievementsData = [
+    { title: "Dejaste de Fumar.", image: "../../assets/imgs/achievement-icons/fumar.png" },//smoke
+    { title: "Traes tus propios cubiertos.", image: "../../assets/imgs/achievement-icons/cubiertos.png" },//cutlery
+    { title: "Usas la bolsa de tela en tus compras.", image: "../../assets/imgs/achievement-icons/bolsa.png" },//bag
+    { title: "Has usado el transporte público.", image: "../../assets/imgs/achievement-icons/bus.png" },//bus
+    { title: "Usas termo en la Universidad.", image: "../../assets/imgs/achievement-icons/termo.png" },//bottle
+    { title: "Reciclaste tus entregas, maquetas y fotocopias.", image: "../../assets/imgs/achievement-icons/maquetas.png" },// photocopies
+    { title: "Llevaste a varios compañeros en tu carro a la Universidad.", image: "../../assets/imgs/achievement-icons/carro.png" },// car 
+    { title: "Dejaste de usar pitillo.", image: "../../assets/imgs/achievement-icons/pitillo.png" },// straw:
+    { title: "Viniste a la Universidad en un transporte alternativo.", image: "../../assets/imgs/achievement-icons/bicicleta.png" },// bicycle:
+    { title: "Usas las escaleras.", image: "../../assets/imgs/achievement-icons/escaleras.png" },// stairs:
+    { title: "Separas adecuadamente tus residuos.", image: "../../assets/imgs/achievement-icons/residuos.png" },// waste:
+    { title: "No usas desechables.", image: "../../assets/imgs/achievement-icons/desechables.png" },// disposable
+    { title: "Imprimes tus trabajos en papel reciclado.", image: "../../assets/imgs/achievement-icons/reciclar.png" }, // paper:
+    { title: "Te bañas en 5 minutos.", image: "../../assets/imgs/achievement-icons/ducha.png" }// shower:
+]
