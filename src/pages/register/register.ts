8ab5bba941b75a8ff378492bea695f5cffe9031b
santisanchez <santisanchez.1214@gmail.com>
2018-05-19 14:20:10 -0500

    Se agregan formularios, estilos y validacion de formulario

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,9 +1,11 @@
-import { UserProvider } from './../../providers/user/user';
+import { FirebaseUserProvider } from './../../providers/firebase-user/firebase-user';
 import { HomePage } from './../home/home';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { RegisterForm_1Page } from '../register-form-1/register-form-1';
-import { User } from '../../modules/user';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import { matchOtherValidator } from '../../modules/matchOther.validator';
 
 @IonicPage()
 @Component({
@@ -12,31 +14,61 @@ import { User } from '../../modules/user';
 })
 export class RegisterPage {
 
-  user = {} as User;
-
   public registerForm1: any = RegisterForm_1Page;
+  public home: any = HomePage;//Temporal
 
-  public home: any = HomePage;
+  formGroup: FormGroup;
+  passwords: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,
-    private userProvider: UserProvider) {
+  public validationMessages: any;
+
+  public email: string;
+  public username: string;
+  public password: string;
+  public passwordConfirm: string;
+
+
+
+  constructor(public navCtrl: NavController, public navParams: NavParams
+    , private formBuilder: FormBuilder, private firebaseUser: FirebaseUserProvider) {
+    this.validationMessages = {};
+    this.setValidations();
   }
 
-  register(user: User) {
-    try {
-      this.userProvider.register(user);
+  register(email, username, password, confirm) {
+    if (this.formGroup.valid) {
+      this.firebaseUser.register(email, password, username);
       this.navCtrl.push(this.home);
-    } catch (e) {
-      console.error(e);
-
     }
   }
 
+  setValidations() {
+    this.formGroup = this.formBuilder.group({
+      email: ['', Validators.compose([Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$'), Validators.required])],
+      username: ['', Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(25)])],
+      password: ['', Validators.compose([Validators.minLength(5), Validators.required])],
+      passwordConfirm: ['', Validators.compose([Validators.minLength(5), Validators.required, matchOtherValidator('password')])]
+    }, { updateOn: 'submit' });
 
-  ionViewDidLoad() {
-
+    this.validationMessages = {
+      email: [
+        { type: 'required', message: 'El correo es requerido.' },
+        { type: 'pattern', message: 'Debes ingresar un correo valido.' },
+      ],
+      username: [
+        { type: 'required', message: 'El usuario es requerido.' },
+        { type: 'minlength', message: 'El usuario debe tener minimo 5 caracteres.' },
+        { type: 'maxlength', message: 'El usuario debe tener maximo 25 caracteres.' },
+      ],
+      password: [
+        { type: 'required', message: 'La contraseña es requerida.' },
+        { type: 'minlength', message: 'La contraseña debe tener almenos 5 caracteres.' },
+      ],
+      passwordConfirm: [
+        { type: 'required', message: 'Confirmar la contraseña es requerido.' },
+        { type: 'minlength', message: 'Confirmar la contraseña debe tener almenos 5 caracteres.' },
+        { type: 'matchOther', message: 'Las contraseñas deben coincidir.' }
+      ]
+    }
   }
-
-
-
 }
