59c9fc6efa79918729e4f324b756e05efc27e9fa
santisanchez <santisanchez.1214@gmail.com>
2018-05-19 14:18:17 -0500

    Se agregan validaciones para el login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,8 +1,9 @@
+import { FirebaseUserProvider } from './../../providers/firebase-user/firebase-user';
 import { MainPage } from './../main/main';
-import { User } from './../../modules/user';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { UserProvider } from '../../providers/user/user';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 
 @IonicPage()
 @Component({
@@ -11,26 +12,61 @@ import { UserProvider } from '../../providers/user/user';
 })
 export class LoginPage {
 
-  user: any;
+
+
   main: any = MainPage;
 
-  constructor(private userProvider: UserProvider,
-    public navCtrl: NavController, public navParams: NavParams) {
-    this.user = {};
+  public email: string;
+  public password: string;
+
+  formGroup: FormGroup;
+
+  public validationMessages: any;
+
+  constructor(private firebaseUser: FirebaseUserProvider, private formBuilder: FormBuilder,
+    public navCtrl: NavController, public navParams: NavParams, private alertCtrl: AlertController) {
+    this.validationMessages = {};
+    this.setValidations();
   }
 
-  public login(user) {
-    this.userProvider.login(user).then(
-      () => {
-        this.navCtrl.push(this.main)
-      }, (reason) => {
-        console.log("the reason of rejection was: " + reason);
+  login(email, password) {
+    if (this.formGroup.valid) {
+      this.firebaseUser.login(email, password, this.onLoginSuccess.bind(this));
+    }
+  }
+
+  onLoginSuccess(isSuccess, rejection?): any {
+    if (isSuccess) {
+      this.navCtrl.push(this.main);
+    } else {
+      let alert = this.alertCtrl.create({
+        title: 'Vuelve a intentarlo',
+        subTitle: 'Correo electronico o Contraseña Incorrecta',
+        buttons: ['Ok'],
+        cssClass: 'custom-alert',
       });
+      alert.present();
+    }
+
   }
 
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
-  }
+  setValidations() {
+    this.formGroup = this.formBuilder.group({
+      email: ['', Validators.compose([Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$'), Validators.required])],
+      password: ['', Validators.compose([Validators.minLength(5), Validators.required])]
+    }, { updateOn: 'submit' });
 
+    this.validationMessages = {
+      email: [
+        { type: 'required', message: 'El correo es requerido.' },
+        { type: 'pattern', message: 'Debes ingresar un correo valido.' },
+      ],
+      password: [
+        { type: 'required', message: 'La contraseña es requerida.' },
+        { type: 'minlength', message: 'La contraseña debe tener almenos 5 caracteres.' },
+        { type: 'invalid', message: 'La contraseña es incorrecta.' }
+      ]
+    }
+  }
 }
